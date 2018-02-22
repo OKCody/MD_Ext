152d3e25423e03a050ebf84646ec93130de0f1d6
Cody Taylor <codyalantaylor@gmail.com>
2018-02-22 07:30:29 -0600

    added 'option-al' invocation of watch()

--- src/content/watch.js
+++ src/content/watch.js
@@ -1,54 +1,65 @@
-var oldContent = "";
-var errorCount = 0;
-var pollingInterval;
+watch();
 
-xhr = new XMLHttpRequest();
-reader = new FileReader();
+function watch(){
+  var oldContent = "";
+  var errorCount = 0;
+  var pollingInterval;
 
-// Run immediately, then once every interval thereafter.
-getFile(document);
-pollingInterval = setInterval(function(){getFile(document)}, 250);
+  xhr = new XMLHttpRequest();
+  reader = new FileReader();
 
-xhr.onload = function(){
-  // Occasionally a read is attempted while a read is in progress. -not under
-  // normal conditions, rather when the machine was put to sleep with the
-  // extension running it will throw an error on wake-up
-  if(reader.readyState != 1){
-    reader.readAsText(xhr.response);
+  // Run immediately, then once every interval thereafter.
+  getFile(document);
+  pollingInterval = setInterval(function(){getFile(document)}, 250);
+
+  chrome.storage.local.get(['options'], function(result){
+    if(result.options.watch != false){
+      // change nothing
+    }
+    else{
+      console.log('watch', result.options.watch);
+      // prevent from calling getFile again
+      clearInterval(pollingInterval);
+    }
+  });
+
+
+  xhr.onload = function(){
+    // Occasionally a read is attempted while a read is in progress. -not under
+    // normal conditions, rather when the machine was put to sleep with the
+    // extension running it will throw an error on wake-up
+    if(reader.readyState != 1){
+      reader.readAsText(xhr.response);
+    }
   }
-}
 
-reader.onload = function(e){
-  if(compare(e.srcElement.result, oldContent)){
-    // Do nothing if no change
+  reader.onload = function(e){
+    if(compare(e.srcElement.result, oldContent)){
+      // Do nothing if no change
+    }
+    else{
+      oldContent = e.srcElement.result;
+      showdownCall(e.srcElement.result);
+      updateMath();
+    }
+    //xhr = null; // Might be a way to mitigate a memory leak
+    //reader = null; // Might be a way to mitigate a memory leak
+    delete xhr.onload;
+    delete reader.onload;
+    delete reader.result;
   }
-  else{
-    oldContent = e.srcElement.result;
-    document.getElementsByTagName('body')[0].innerHTML = showdownCall(e.srcElement.result);
-    /*
-    chrome.tabs.executeScript(tabId, {
-      code:"MathJax.Hub.Queue(['Typeset',MathJax.Hub])"
-    });
-    */
-    updateMath();
-  }
-  //xhr = null; // Might be a way to mitigate a memory leak
-  //reader = null; // Might be a way to mitigate a memory leak
-  delete xhr.onload;
-  delete reader.onload;
-  delete reader.result;
-}
 
-// In the event that the content is not able to be loaded after 5 attempts
-// exit polling loop with error.
-xhr.onerror = function(){
-  console.log("XHR Error");
-  if(errorCount >=4){
-    clearInterval(pollingInterval); // Stops polling on 5th error
-    console.log("Markdown file was possibly deleted.");
-    console.log("Polling stopped. Refresh content page to restart.");
+  // In the event that the content is not able to be loaded after 5 attempts
+  // exit polling loop with error.
+  xhr.onerror = function(){
+    console.log("XHR Error");
+    if(errorCount >=4){
+      clearInterval(pollingInterval); // Stops polling on 5th error
+      console.log("Markdown file was possibly deleted.");
+      console.log("Polling stopped. Refresh content page to restart.");
+    }
+    errorCount = errorCount + 1;
   }
-  errorCount = errorCount + 1;
 }
 
 function getFile(document){
@@ -73,9 +84,16 @@ function compare(content, oldContent){
 }
 
 function showdownCall(newContent){
-	var converter = new showdown.Converter();
-	var html = converter.makeHtml(newContent);
-	return html;
+  chrome.storage.local.get(['options'], function(result){
+    if(result.options.markdown != false){
+      var converter = new showdown.Converter();
+    	var html = converter.makeHtml(newContent);
+    	document.getElementsByTagName('body')[0].innerHTML = html;
+    }
+    else{
+      console.log('markdown', result.options.markdown);
+    }
+  });
 }
 
 function isAllowedURL(page){
@@ -94,15 +112,22 @@ function isAllowedURL(page){
 }
 
 function updateMath(){
-  if(document.getElementById('re-math')){
-    document.getElementById('re-math').remove();
-  }
-  if(document.getElementById('mathjaxReady')){
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.id = "re-math";
-    script.innerHTML = "MathJax.Hub.Queue(['Typeset',MathJax.Hub]);";
-    script.setAttribute('async','');
-    document.getElementsByTagName("head")[0].appendChild(script);
-  }
+  chrome.storage.local.get(['options'], function(result){
+    if(result.options.mathjax != false){
+      if(document.getElementById('re-math')){
+        document.getElementById('re-math').remove();
+      }
+      if(document.getElementById('mathjaxReady')){
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.id = "re-math";
+        script.innerHTML = "MathJax.Hub.Queue(['Typeset',MathJax.Hub]);";
+        script.setAttribute('async','');
+        document.getElementsByTagName("head")[0].appendChild(script);
+      }
+    }
+    else{
+      console.log('watch_mathjax', result.options.mathjax);
+    }
+  });
 }
